329c09bedb66bd2d0c1e61e3fece6632a21c6207
hao <howine66@gmail.com>
2019-07-22 20:41:58 -0500

    homepage into components and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage.styles.scss';
+
+const Homepage = () => (
+  <Directory/>
+)
+
+export default Homepage;
