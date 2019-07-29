f303867269e61dc15f0b9c725259fdbad87addd9
hao <howine66@gmail.com>
2019-07-29 16:52:22 -0500

    SSS

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,37 @@ const config = {
   };
 
   firebase.initializeApp(config);
-  export const auth =firebase.auth();
-  export const firestore =firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  provider.setCustomParameters({ prompt : 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  export default firebase;
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
