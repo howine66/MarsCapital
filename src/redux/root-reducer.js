eca1dc7ba528def481e39979566b496967c1da09
haomac <howine66@gmail.com>
2019-08-03 00:55:19 -0500

    add cart-icon and cart-dropdown

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,8 @@
 import {combineReducers }from 'redux';
 import userReducer from './user/user.reducer';
-
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers( {
+    cart: cartReducer,
     user: userReducer
 });
